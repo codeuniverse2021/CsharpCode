0b232def92fa6c56936abb0a6eef66613edbd17c
codeuniverse2021 <durgeshrwat1700@gmail.com>
2022-06-19 13:05:26 +0530

    COde of Csharp

---
+++ FuntionUnderstanding_2/functionUnderstanding_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// function name parameter with no return type
+namespace FuntionUnderstanding_2
+{
+    class functionUnderstanding_2
+    {
+        int i, fact=1, n;
+        void fun( int s)
+        {
+            for (i = 1; i <= s; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("the value of given factorial" +" "+n+ " is" +" "+ fact);
+        }
+        static void Main(string[] args)
+        {
+            functionUnderstanding_2 obj = new functionUnderstanding_2();
+            Console.WriteLine("Please enter the no for factorial :");
+            obj.n = Convert.ToInt32(Console.ReadLine());
+            obj.fun(obj.n);
+
+          
+        }
+    }
+}
